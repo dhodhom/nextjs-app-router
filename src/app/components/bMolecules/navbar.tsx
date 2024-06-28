d0b97d3ff13866906154647192d8023eb6d0d65a
unknown <dhodhom@gmail.com>
2024-06-28 11:23:47 +0700

    3 commit

---
+++ src/app/components/bMolecules/navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="flex bg-gray-800 py-2 px-5">
+      <h1 className="text-white">Navbar</h1>
+      <ul className="flex">
+        <li className="">Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
